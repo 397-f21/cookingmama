2abb241d21e4823e1fc20a32222fdfb17dfc23f8
zacdeng <zacdeng0720@gmail.com>
2021-10-05 18:48:06 -0500

    Complete Recipes API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+// import { RecipesData } from './utilities/defaultData';
 import './App.css';
-
-const RecipesData = {
-  "title" : "Open Recipes Database",
-  "recipes" : {
-    "S1" : {
-      "id" : "S1",
-      "title" : "Apple Curry Turkey Pita",
-      "imageURL" : "https://i.loli.net/2021/10/04/LZ582sSwITXf9Cc.png"
-    },
-    "S2" : {
-      "id" : "S2",
-      "title" : "BBQ Tuna Fritters",
-      "imageURL" : "https://i.loli.net/2021/10/04/Em41W2oJjRNfwQv.png"
-    },
-    "S3" : {
-      "id" : "S3",
-      "title" : "Lemon Chicken Orzo Soup",
-      "imageURL" : "https://i.loli.net/2021/10/04/NdZLKAJtlPRC9ar.png"
-    }
-  }
-};
 
 const Frame = {
   title: "Cooking MaMa ",
@@ -39,24 +20,53 @@ const Banner = ({title, subTitle}) => (
 const Recipe = ({ recipe }) => (
   <div className="card m-1 p-2">
     <div className="card-body">
-      <div className="card-imgURL"><p><img src={ recipe.imageURL } style={{width:300, height:300}}/></p></div>
-      <div className="card-title">{ recipe.title }</div>
+      <div className="card-imgURL"><p><img src={ recipe.image } alt={ "recipeImage" } style={{width:300, height:300}}/></p></div>
+      <div className="card-title">{ recipe.label }</div>
     </div>
   </div>
 );
 
 const RecipeList = ({ recipes }) => (
   <div className="recipe-list">
-    { Object.values(recipes).map(recipe => <Recipe key={ recipe.id } recipe={ recipe } />) }
+    { Object.values(recipes).map(recipe => <Recipe key={ recipe.recipe.label } recipe={ recipe.recipe } />) }
   </div>
 );
 
-const App = () => (
-  <div className="container">
+const App = () => {
+  const [recipesAPI, setRecipesAPI] = useState();
+  // At least four parameters required : type / q / app_id / app_key
+  const type = 'public';
+  var q = 'soup';
+  const app_id = 'f875478e';
+  const app_key = 'b4b36939c6bb8d8c11b08248abf1b86d';
+  const random = 'true';
+  const url = 'https://api.edamam.com/api/recipes/v2?type=' + type + '&q=' + q + '&app_id=' +
+   app_id + '&app_key=' + app_key + '&random' + random;
+
+  useEffect(() => {
+      const fetchSchedule = async () => {
+        const response = await fetch(url);
+        if (!response.ok) throw response;
+        const json = await response.json();
+        setRecipesAPI(json);
+      }
+      fetchSchedule();
+      console.log(JSON.stringify(recipesAPI));
+      // fetch(url)
+      // .then((response) => setRecipesAPI(response.json()))
+      // .then((response) => console.log(response.json.stringify(recipesAPI)))
+    }, []);  // useEffect((),[]) make sure that only fetch once during the first time loading the website page
+  
+  if (!recipesAPI) return <h1>Loading...</h1>;
+
+  return (
+    <div className="container">
     <Banner title={ Frame.title } subTitle={ Frame.subTitle } />
-    <RecipeList recipes={ RecipesData.recipes } />
+    <RecipeList recipes={ recipesAPI.hits } />
+    <p></p>
     <button className="btn btn-outline-success btn-lg"> New Recipe </button>
   </div>
-);
+  );
+};
 
 export default App;
---
+++ src/utilities/defaultData.js
@@ -0,0 +1,22 @@
+
+// default data
+export const RecipesData = {
+  "title" : "Open Recipes Database",
+  "recipes" : {
+    "S1" : {
+      "id" : "S1",
+      "title" : "Apple Curry Turkey Pita",
+      "imageURL" : "https://i.loli.net/2021/10/04/LZ582sSwITXf9Cc.png"
+    },
+    "S2" : {
+      "id" : "S2",
+      "title" : "BBQ Tuna Fritters",
+      "imageURL" : "https://i.loli.net/2021/10/04/Em41W2oJjRNfwQv.png"
+    },
+    "S3" : {
+      "id" : "S3",
+      "title" : "Lemon Chicken Orzo Soup",
+      "imageURL" : "https://i.loli.net/2021/10/04/NdZLKAJtlPRC9ar.png"
+    }
+  }
+};
